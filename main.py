072facb170f6fecfc93d2913d8de9d4144b01ec4
Jetesada Kaewmanee <lo6ster6@gmail.com>
2024-01-05 21:21:55 +0700

    add function solve(inprogress)

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Math24Solver(App):
             self.numbers_input.append(number_input)
             layout.add_widget(number_input)
 
-        solve_button = Button(text='Solve')
+        solve_button = Button(text='Solve', on_press=self.solve)
         layout.add_widget(solve_button)
 
         return layout
@@ -28,6 +28,26 @@ class Math24Solver(App):
             return eval(expr) == 24
         except ZeroDivisionError:
             return False
+
+    def find_24_solutions(self, numbers):
+        operators = ['+', '-', '*', '/']
+        for perm in permutations(numbers):
+            for ops in product(operators, repeat=3):
+                expr = f"(({perm[0]}{ops[0]}{perm[1]}){ops[1]}{perm[2]}){ops[2]}{perm[3]}"
+                if self.evaluate_expression(expr):
+                    return expr
+        return None
+
+    def solve(self, instance):
+        user_numbers = [int(num_input.text) for num_input in self.numbers_input if num_input.text.isdigit()]
+        if len(user_numbers) == 4:
+            solution = self.find_24_solutions(user_numbers)
+            if solution:
+                self.solution_label.text = f"A solution to reach 24: {solution} = 24"
+            else:
+                self.solution_label.text = "No solution found for 24."
+        else:
+            self.solution_label.text = "Please enter 4 valid numbers."
 
 if __name__ == '__main__':
     Math24Solver().run()
